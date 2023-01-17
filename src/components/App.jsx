14551a32e98fe1b59ba2151f3aea7da3757fd5b2
Valeria Likso <valerijasergievich@gmail.com>
2023-01-17 16:07:22 +0000

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,3 @@
-import { ContactForm } from './ContactForm/ContactForm';
-import { ContactList } from './ContactList/ContactList';
-import { Section } from './Section/Section';
-import { Filter } from './Filter/Filter';
 
-const App = () => {
-  return (
-    <div>
-      <Section>
-        <h1>Phonebook</h1>
-        <ContactForm />
-      </Section>
-      <Section>
-        <h2>Contacts</h2>
-        <Filter />
-        <ContactList />
-      </Section>
-    </div>
-  );
-};
 
-export default App;
+
--- src/components/Loader/Loader.styled.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const LoaderStyled = styled.div`
-  display: flex;
-  justify-content: center;
-`;
